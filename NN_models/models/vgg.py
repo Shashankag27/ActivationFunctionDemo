2960e0bd4cd82de196c9586962f63d152f110160
Shashankag27 <32036847+Shashankag27@users.noreply.github.com>
2020-04-28 00:35:43 +0530

    Update vgg.py

--- NN_models/models/vgg.py
+++ NN_models/models/vgg.py
@@ -1,5 +1,6 @@
 '''VGG11/13/16/19 in Pytorch.'''
 import torch
+import numpy as np
 import torch.nn as nn
 import NN_models.ops as ops
 from torch.nn.modules.module import Module
@@ -11,12 +12,22 @@ cfg = {
     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
+def swish(x): 
+    z = 1/(1 + np.exp(-x))
+    return x*z
+
+class Swish(Module):
+    def forward(self, input):
+        return swish(input)
+
 
 class VGG(nn.Module):
     def __init__(self, vgg_name):
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
         self.classifier = nn.Linear(512, 10)
+
+       
 
     def forward(self, x):
         out = self.features(x)
@@ -33,8 +44,7 @@ class VGG(nn.Module):
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
                            nn.BatchNorm2d(x),
-                           nn.SELU(inplace=True)]
+                           Swish()]
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
-
